632226110e3d4e1474db7fdc7f3b5536605efa1f
ronaldo-lourenco <ronaldo.lourenco@escola.pr.gov.br>
2024-10-18 00:00:27 +0000

    Atualizei arquivo de informacoesGlobais.js

---
+++ graficos/informacoesGlobais.js
@@ -0,0 +1,8 @@
+const url='https://raw.githubusercontent.com/silviosnjr/CienciaDeDados-CriandoGraficosDinamicosComJavaScript/refs/heads/Aula01/educacao/educacao-dados-globais.json'
+
+async function vizualizarInformacoesGlobais() {
+    const res = await fetch(url)
+    const dados = await res.json()
+    console.log(dados)
+}
+vizualizarInformacoesGlobais()
