f278f605323aad91055f20ab47aeb47a9fbd4a22
ronaldo-lourenco <ronaldo.lourenco@escola.pr.gov.br>
2024-10-30 17:53:38 +0000

    Atualizar Arquivos Informações Globais e Index.html

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -1,8 +1,16 @@
 const url='https://raw.githubusercontent.com/silviosnjr/CienciaDeDados-CriandoGraficosDinamicosComJavaScript/refs/heads/Aula01/educacao/educacao-dados-globais.json'
 
-async function vizualizarInformacoesGlobais() {
+async function visualizarInformacoesGlobais() {
     const res = await fetch(url)
     const dados = await res.json()
     console.log(dados)
 }
-vizualizarInformacoesGlobais()
+
+const paragrafo = document.createElement('p')
+paragrafo.classList.add('graficos-container__texto')
+paragrafo.innerHTML = `Você sabia que o mundo tem <span> ${dados.total_pessoas_mundo} </span> de pessoas e que aproximadamente <span> ${dados.total_pessoas_empregadas} </span> estão empregadas em serviços registrados. Eles trabalham <span> ${dados.tempo_medio_trabalho_por_semana} </span> horas por semana.`
+const container = document.getElementById('graficos-container')
+container.appendChild(paragrafo);
+
+
+visualizarInformacoesGlobais()
