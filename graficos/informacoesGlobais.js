22633551fb769a0ea5c7662106e33db56383e3e0
ronaldo-lourenco <ronaldo.lourenco@escola.pr.gov.br>
2024-10-30 21:05:15 +0000

    Atualizar arquivos

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -4,13 +4,13 @@ async function visualizarInformacoesGlobais() {
     const res = await fetch(url)
     const dados = await res.json()
     console.log(dados)
-}
+
 
 const paragrafo = document.createElement('p')
 paragrafo.classList.add('graficos-container__texto')
 paragrafo.innerHTML = `Você sabia que o mundo tem <span> ${dados.total_pessoas_mundo} </span> de pessoas e que aproximadamente <span> ${dados.total_pessoas_empregadas} </span> estão empregadas em serviços registrados. Eles trabalham <span> ${dados.tempo_medio_trabalho_por_semana} </span> horas por semana.`
 const container = document.getElementById('graficos-container')
 container.appendChild(paragrafo);
-
+}
 
 visualizarInformacoesGlobais()
