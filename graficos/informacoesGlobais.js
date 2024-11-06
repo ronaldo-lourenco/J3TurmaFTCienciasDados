5e13ea34e473d3f2a3a61294ba1ee6597cf61098
ronaldo-lourenco <ronaldo.lourenco@escola.pr.gov.br>
2024-11-06 20:05:31 +0000

    Atualizei informacoesGlobais

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -3,15 +3,14 @@ const url='https://raw.githubusercontent.com/silviosnjr/CienciaDeDados-CriandoGr
 async function visualizarInformacoesGlobais() {
     const res = await fetch(url)
     const dados = await res.json()
-    console.log(dados)
-    const pessoasConectadas = (dados.total_pessoas_conectadas/1e9) 
+    const pessoasConectadas = (dados.total_pessoas_com_acesso_a_educacao/1e9) 
     const pessoasNoMundo = (dados.total_pessoas_mundo/1e9) 
     const horas = parseInt(dados.tempo_medio_dia_estudando)
     const minutos = Math.round((dados.tempo_medio_dia_estudando - horas)*60)
 
     const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos-container__texto')
-    paragrafo.innerHTML = `Você sabia que o mundo tem <span> ${dados.total_pessoas_mundo} </span> de pessoas e que aproximadamente <span> ${dados.total_pessoas_com_acesso_a_educacao} </span> estão empregadas em serviços registrados. Eles trabalham <span> ${dados.tempo_medio_dia_estudando} </span> horas por semana.`
+    paragrafo.innerHTML = `Você sabia que o mundo tem <span> ${pessoasNoMundo} </span> de pessoas e que aproximadamente <span> ${pessoasConectadas} </span> estão empregadas em serviços registrados. Eles trabalham <span> ${horas} </span> horas e <span>${minutos}</span> minutos por semana.`
     const container = document.getElementById('graficos-container')
     container.appendChild(paragrafo);
 }
